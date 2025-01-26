d9d4344b99b00b59c580995500d25ddf65a3f5c2
nzrnk <a.nzrnk@gmail.com>
2025-01-26 11:41:08 +0300

    new test

--- demotest.spec.js
+++ demotest.spec.js
@@ -2,4 +2,5 @@ import { test, expect } from '@playwright/test';
 
 test('demo', async({page}) => {
     await page.goto('https://realworld.qa.guru/');
+    await page.getByText('Egypt').click();
 })
